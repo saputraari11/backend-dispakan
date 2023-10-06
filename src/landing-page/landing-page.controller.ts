22059af2133e0a227e98e9740cd2b243a8baa794
alipw <alifwide.workmail@gmail.com>
2023-10-06 20:48:50 +0700

    update-swagger-docs (remove auth for landing page)

--- src/landing-page/landing-page.controller.ts
+++ src/landing-page/landing-page.controller.ts
@@ -21,42 +21,35 @@ export class LandingPageController {
   constructor(private landingPageService: LandingPageService) {}
 
   @Get('product')
-  @ApiBearerAuth()
-  // @UseGuards(AuthGuard('jwt'))
   async allProductByStore(@Query() filter: FilterAllProducts) {
     const result = await this.landingPageService.allProductByStore(filter)
     return result
   }
 
   @Get('product/detail/:id')
-  @ApiBearerAuth()
   async detailProdcut(@Param('id') id: string) {
     const result = await this.landingPageService.detailProduct(id)
     return result
   }
 
   @Post('/action')
-  @ApiBearerAuth()
   async actionLandingPage(@Body() incrementDto: IncrementDto) {
     return this.landingPageService.incrementProperty(incrementDto)
   }
 
   @Get('news')
-  @ApiBearerAuth()
   async allNews(@Query() filterDto: FilterAllNews) {
     const result = await this.landingPageService.allNews(filterDto)
     return result
   }
 
   @Get('news/detail/:id')
-  @ApiBearerAuth()
   async detailNews(@Param('id') id: string) {
     const result = await this.landingPageService.detailNews(id)
     return result
   }
 
   @Post('news/comment')
-  @ApiBearerAuth()
   async uploadFile(@Body() data: CreateCommantDto) {
     const result = await this.landingPageService.uploadComment(data)
     return result
